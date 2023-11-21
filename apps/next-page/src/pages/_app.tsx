fb1d71decf1d709cf96cedcba32f0823ad0843fd
lesenelir <hi@lesenelir.me>
2023-11-21 22:22:40 +0800

    feat: pages router i18n

--- apps/next-page/src/pages/_app.tsx
+++ apps/next-page/src/pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 import { ThemeProvider } from 'next-themes'
+import { NextIntlClientProvider } from 'next-intl'
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <NextIntlClientProvider
+      locale={router.locale}
+      messages={pageProps.messages}
+    >
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </NextIntlClientProvider>
   )
 }
--- apps/next-page/src/pages/index.tsx
+++ apps/next-page/src/pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { useTheme } from 'next-themes'
+import { useRouter } from 'next/router'
+import { useTranslations } from 'next-intl'
+import type { GetStaticPropsContext } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const {theme, setTheme} = useTheme()
+  const router = useRouter()
+  const t = useTranslations()
 
   return (
     <>
@@ -21,21 +26,39 @@ export default function Home() {
         dark:bg-black dark:text-white ${inter.className}
       `}
       >
-        <p>Pages Router - Hello World</p>
+        <p>{t('homepage.title')}</p>
 
-        <div className={'mt-4'}>
+        <div className={'mt-4 flex flex-row gap-4'}>
           <button
             className={`
-            p-2 bg-gray-50 rounded-lg shadow text-black 
+            w-28 p-2 bg-gray-50 rounded-lg shadow text-black 
             focus:ring-1 focus:ring-black focus:ring-opacity-7 hover:bg-gray-100
           `}
             onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
           >
             Theme
           </button>
+          <button
+            className={`
+            w-28 p-2 bg-gray-50 rounded-lg shadow text-black 
+            focus:ring-1 focus:ring-black focus:ring-opacity-7 hover:bg-gray-100
+          `}
+            onClick={() => router.push(router.pathname, router.asPath,{locale: router.locale === 'en' ? 'zh' : 'en'})}
+          >
+            International
+          </button>
         </div>
       </div>
     </>
 
   )
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+
+  return {
+    props: {
+      messages: (await import(`../../locales/${context.locale}.json`)).default
+    }
+  }
 }
