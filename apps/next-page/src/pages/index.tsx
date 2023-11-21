4b64255f71dcdffd615d9e8471091bf3ec7afea3
lesenelir <hi@lesenelir.me>
2023-11-21 18:06:28 +0800

    fix: correct icon display

--- apps/next-page/src/pages/index.tsx
+++ apps/next-page/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import { useTheme } from 'next-themes'
 
@@ -7,25 +8,34 @@ export default function Home() {
   const {theme, setTheme} = useTheme()
 
   return (
-    <div
-      className={`
+    <>
+      <Head>
+        <title>Create Next Page</title>
+        <meta name='description' content='Create Next Page'/>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div
+        className={`
         flex flex-col justify-center items-center min-h-screen 
         dark:bg-black dark:text-white ${inter.className}
       `}
-    >
-      <p>Pages Router - Hello World</p>
+      >
+        <p>Pages Router - Hello World</p>
 
-      <div className={'mt-4'}>
-        <button
-          className={`
+        <div className={'mt-4'}>
+          <button
+            className={`
             p-2 bg-gray-50 rounded-lg shadow text-black 
             focus:ring-1 focus:ring-black focus:ring-opacity-7 hover:bg-gray-100
           `}
-          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        >
-          Theme
-        </button>
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          >
+            Theme
+          </button>
+        </div>
       </div>
-    </div>
+    </>
+
   )
 }
